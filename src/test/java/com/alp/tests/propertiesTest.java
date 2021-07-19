ba976693c1b33c7653da5c864f0e579d37098d0e
anonimid <freelancetradertr@gmail.com>
2021-07-20 02:52:05 +0300

    Initial 2

--- src/test/java/com/alp/tests/propertiesTest.java
+++ src/test/java/com/alp/tests/propertiesTest.java
@@ -1,12 +1,7 @@
 package com.alp.tests;
 
 import com.alp.utilities.ConfigurationReader;
-import com.alp.utilities.Driver;
 import com.alp.utilities.TestBase;
-import com.alp.utilities.WebDriverFactory;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
 
 public class propertiesTest extends TestBase {
